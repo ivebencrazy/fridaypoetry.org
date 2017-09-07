143b1c841966f3f37fa3f2719d549dfa32583b8a
ivebencrazy <ivebencrazy@users.noreply.github.com>
2017-09-07 09:31:46 -0700

    Use lodash-es like all the cool kids

--- preact.config.js
+++ preact.config.js
@@ -1,14 +1,13 @@
 import { resolve } from "path";
-import preactCliLodash from "preact-cli-lodash";
 import preactCliTypeScript from "preact-cli-plugin-typescript";
 
 export default function (config, env, helpers) {
   // Use typescript
-  config = preactCliLodash(config);
   config = preactCliTypeScript(config);
 
   // Start by looking at index.tsx
   config.resolve.alias["preact-cli-entrypoint"] = resolve(__dirname, "src", "index.tsx");
+  config.resolve.alias["lodash"] = resolve(__dirname, "node_modules", "lodash-es")
 
   return config;
 }
