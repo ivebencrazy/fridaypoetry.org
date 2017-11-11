ffe6db3cfd3395837f9295e0059d4ba5fc4f3b7e
ivebencrazy <ivebencrazy@users.noreply.github.com>
2017-11-11 10:19:01 -0800

    Adds store test files

---
+++ src/store/reducers/reducers.test.ts
@@ -0,0 +1,5 @@
+import reducer from "./reducers";
+
+
+test("FETCH_IDS_SUCCESS should update ids");
+test("FETCH_POEM_SUCCESS should update poems");
---
+++ src/store/services/services.test.ts
@@ -0,0 +1,16 @@
+import { createPoem, fetchPoem, fetchPoemIds } from "./services";
+
+
+describe("createPoem", () => {
+  test("Should create a poem if it's friday");
+  test("Should fail if it's not friday");
+});
+
+describe("fetchPoemIds", () => {
+  test("Should fetch poem ids");
+  test("Ids should be from most recent first");
+});
+
+describe("fetchPoem", () => {
+  test("Should fetch poems");
+});
